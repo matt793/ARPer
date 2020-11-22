93d6ed16ec3ad1a859df984a1a7165140aa46e5d
Matthew Lopez <61769072+matt793@users.noreply.github.com>
2020-11-22 13:56:38 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 sudo apt-get install wireshark
 sudo apt-get install nmap
-sudo apt-get install ettercap-common
+sudo apt-get install install ettercap-graphical 
 cd /usr/bin/
 sudo bash 
 chmod 755 dumpcap
+exit
+exit
